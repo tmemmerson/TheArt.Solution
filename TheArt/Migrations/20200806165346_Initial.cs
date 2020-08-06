5fafd1055a88441b91d6bed9860609e587414733
Tristan Emmerson <tristan@stickerslug.com>
2020-08-06 11:49:09 -0700

    update.

---
+++ TheArt/Migrations/20200806165346_Initial.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheArt.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ArtistName = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    DateOfDeath = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movements",
+                columns: table => new
+                {
+                    MovementId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    MovementName = table.Column<string>(nullable: true),
+                    MovementDescription = table.Column<string>(nullable: true),
+                    MovementStart = table.Column<DateTime>(nullable: false),
+                    MovementEnd = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movements", x => x.MovementId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pieces",
+                columns: table => new
+                {
+                    PieceId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PieceName = table.Column<string>(nullable: true),
+                    PieceDate = table.Column<DateTime>(nullable: false),
+                    PieceImage = table.Column<byte[]>(nullable: true),
+                    ArtistId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pieces", x => x.PieceId);
+                    table.ForeignKey(
+                        name: "FK_Pieces_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArtistMovement",
+                columns: table => new
+                {
+                    ArtistMovementId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ArtistId = table.Column<int>(nullable: false),
+                    MovementId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistMovement", x => x.ArtistMovementId);
+                    table.ForeignKey(
+                        name: "FK_ArtistMovement_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArtistMovement_Movements_MovementId",
+                        column: x => x.MovementId,
+                        principalTable: "Movements",
+                        principalColumn: "MovementId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistMovement_ArtistId",
+                table: "ArtistMovement",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistMovement_MovementId",
+                table: "ArtistMovement",
+                column: "MovementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pieces_ArtistId",
+                table: "Pieces",
+                column: "ArtistId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArtistMovement");
+
+            migrationBuilder.DropTable(
+                name: "Pieces");
+
+            migrationBuilder.DropTable(
+                name: "Movements");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
