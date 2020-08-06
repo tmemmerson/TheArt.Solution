9832f0131d5842485563c2195f3902e68108a9b6
Tristan Emmerson <tristan@stickerslug.com>
2020-08-06 11:47:37 -0700

    update.

--- TheArt/Controllers/ArtistsController.cs
+++ TheArt/Controllers/ArtistsController.cs
@@ -3,6 +3,7 @@ using TheArt.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace TheArt.Controllers
 {
@@ -10,15 +11,106 @@ namespace TheArt.Controllers
   {
     private readonly TheArtContext _db;
 
-    public ArtistsController(TheArtContext db)
+    public ArtistsController(TheArtContext db) //good
     {
       _db = db;
     }
 
-    public ActionResult Index()
+    public ActionResult Index() //good
     {
-      List<Artist> model = _db.Artists.ToList();
+      List<Artist> model = _db.Artists.ToList(); //create list out of database table called artists
       return View(model);
     }
+
+    public ActionResult Create() //good
+    {
+      ViewBag.MovementId = new SelectList(_db.Movements, "MovementId", "MovementName");
+      return View();
+    }
+
+    [HttpPost]
+
+    public ActionResult Create(Artist artist, int MovementId) //good
+    {
+      _db.Artists.Add(artist); // if conditional statement below is not true then just add artist object to list of artists
+      if(MovementId != 0)
+      {
+        _db.ArtistMovement.Add(new ArtistMovement() {MovementId = MovementId, ArtistId = artist.ArtistId}); // we did artist.ArtistId because we passed the artist object as an argument to create so we have to go inside and grab the ArtistId
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id) //good
+    {
+      var thisArtist = _db.Artists
+        .Include(artist => artist.Movements)
+        .ThenInclude(join => join.Movement)
+        .FirstOrDefault(artist => artist.ArtistId == id);
+      return View(thisArtist);
+    }
+
+
+    public ActionResult Edit(int id) //good
+    {
+      var thisArtist = _db.Artists.FirstOrDefault(artists => artists.ArtistId == id);
+      ViewBag.MovementId = new SelectList(_db.Movements, "MovementId", "MovementName");
+      return View(thisArtist);
+    }
+  
+    [HttpPost]
+    public ActionResult Edit(Artist artist, int MovementId) //good
+    {
+      if (MovementId != 0)
+      {
+        _db.ArtistMovement.Add(new ArtistMovement() { MovementId = MovementId, ArtistId = artist.ArtistId });
+      }
+      _db.Entry(artist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id) //good
+    {
+      var thisArtist = _db.Artists.FirstOrDefault(artists => artists.ArtistId == id);
+      return View(thisArtist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id) //good
+    {
+      var thisArtist = _db.Artists.FirstOrDefault(artists => artists.ArtistId == id);
+      _db.Artists.Remove(thisArtist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteMovement(int joinId) // good
+    {
+      var joinEntry = _db.ArtistMovement.FirstOrDefault(entry => entry.ArtistMovementId == joinId); // seperates the join table so you can remove a movement
+      _db.ArtistMovement.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+    public ActionResult AddMovement(int id) //good
+    {
+      var thisArtist = _db.Artists.FirstOrDefault(artists => artists.ArtistId == id);
+      ViewBag.MovementId = new SelectList(_db.Movements, "MovementId", "MovementName");
+      return View(thisArtist);
+    }
+
+    [HttpPost]
+    public ActionResult AddMovement(Artist artist, int MovementId) //good
+    {
+      if (MovementId != 0)
+      {
+        _db.ArtistMovement.Add(new ArtistMovement() { MovementId = MovementId, ArtistId = artist.ArtistId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
